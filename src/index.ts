10bd0035a620bde12aad6a56ea68ef00ac76848d
Sébastien MANICON <smanicon@free.fr>
2022-10-02 09:49:19 +0200

    extract model data

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { mat4 } from 'gl-matrix'
 import vertexShaderSource from './shader/vertex.vert'
 import fragmentShaderSource from './shader/fragment.frag'
+import { triangleVertices, triangleVerticesColors, triangleVertexIndices } from './model/triangular_prism'
 
 function main (): void {
   const canvas: HTMLCanvasElement = document.querySelector('#gl_canvas') as HTMLCanvasElement
@@ -17,72 +18,13 @@ function main (): void {
   gl.linkProgram(glProgram)
   gl.useProgram(glProgram)
 
-  const triangleVertices = [
-    // front face
-    // bottom left to right, to top
-    0.0, 0.0, 0.0,
-    1.0, 0.0, 0.0,
-    2.0, 0.0, 0.0,
-    0.5, 1.0, 0.0,
-    1.5, 1.0, 0.0,
-    1.0, 2.0, 0.0,
-    // rear face
-    0.0, 0.0, -2.0,
-    1.0, 0.0, -2.0,
-    2.0, 0.0, -2.0,
-    0.5, 1.0, -2.0,
-    1.5, 1.0, -2.0,
-    1.0, 2.0, -2.0
-  ]
   const trianglesVerticeBuffer: WebGLBuffer = gl.createBuffer() as WebGLBuffer
   gl.bindBuffer(gl.ARRAY_BUFFER, trianglesVerticeBuffer)
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW)
 
-  const triangleVerticeColors = [
-    // front face
-    0.0, 0.0, 1.0,
-    1.0, 1.0, 1.0,
-    0.0, 0.0, 1.0,
-    0.0, 0.0, 1.0,
-    0.0, 0.0, 1.0,
-    1.0, 1.0, 1.0,
-    // rear face
-    0.0, 1.0, 1.0,
-    1.0, 1.0, 1.0,
-    0.0, 1.0, 1.0,
-    0.0, 1.0, 1.0,
-    0.0, 1.0, 1.0,
-    1.0, 1.0, 1.0
-  ]
   const trianglesColorBuffer: WebGLBuffer = gl.createBuffer() as WebGLBuffer
   gl.bindBuffer(gl.ARRAY_BUFFER, trianglesColorBuffer)
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVerticeColors), gl.STATIC_DRAW)
-
-  const triangleVertexIndices = [
-    // front face
-    0, 1, 3,
-    1, 3, 4,
-    1, 2, 4,
-    3, 4, 5,
-    // rear face
-    6, 7, 9,
-    7, 9, 10,
-    7, 8, 10,
-    9, 10, 11,
-    // left side
-    0, 3, 6,
-    3, 6, 9,
-    3, 5, 9,
-    5, 9, 11,
-    // right side
-    2, 4, 8,
-    4, 8, 10,
-    4, 5, 10,
-    5, 10, 11,
-    // bottom faces
-    0, 6, 8,
-    8, 2, 0
-  ]
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVerticesColors), gl.STATIC_DRAW)
 
   const trianglesIndiceBuffer: WebGLBuffer = gl.createBuffer() as WebGLBuffer
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, trianglesIndiceBuffer)
